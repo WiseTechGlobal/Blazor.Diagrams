09d7209a5f742fd2d4656155630ca6d1c5f866f8
Heather Cox <Heather.Cox@wisetechglobal.com>
2023-06-14 17:42:29 +1000

    add brutal.dev.strongnamesigner, add nuspec

--- PushNuget.ps1
+++ PushNuget.ps1
@@ -3,7 +3,7 @@ pushd $PSScriptRoot
 $feedSource = "http://proget.wtg.zone/nuget/WTG-Internal/"
 $apiKey = ""
 # $packagePath = "C:\git\wtg\Blazor.Diagrams\src\Blazor.Diagrams.Core\bin\Debug\WTG.Z.Blazor.Diagrams.Core.1.0.2.nupkg"
-$packagePath = "C:\git\wtg\Blazor.Diagrams\src\Blazor.Diagrams\bin\Debug\WTG.Z.Blazor.Diagrams.1.0.2.nupkg"
+$packagePath = "C:\git\wtg\Blazor.Diagrams\src\Blazor.Diagrams\bin\Release\WTG.Z.Blazor.Diagrams.1.0.6.nupkg"
 
 function Write-Log {
     Write-Host "$(get-date -f "yyyy-MM-dd HH:mm:ss.fff")`t$args"
