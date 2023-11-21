87a7ae466545d5d4910821432df9ee9ddd733109
Heather Cox <Heather.Cox@wisetechglobal.com>
2023-11-21 16:42:23 +1100

    add widget test

---
+++ tests/Blazor.Diagrams.Tests/Components/Controls/ResizeControlWidgetTests.cs
@@ -0,0 +1,25 @@
+﻿using Blazor.Diagrams.Components.Controls;
+using Blazor.Diagrams.Components.Renderers;
+using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blazor.Diagrams.Tests.Components.Controls
+{
+    public class ResizeControlWidgetTests
+    {
+        [Fact]
+        public void ShouldRenderDiv()
+        {
+            using var ctx = new TestContext();
+
+            var cut = ctx.RenderComponent<ResizeControlWidget>();
+
+            cut.MarkupMatches("<div class=\"default-node-resizer\" />");
+        }
+    }
+}
