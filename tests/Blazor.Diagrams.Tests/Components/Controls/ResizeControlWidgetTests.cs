7a0872c29594d51f98ee7df9678cc42879661d7d
Heather Cox <Heather.Cox@wisetechglobal.com>
2023-12-07 15:13:26 +1100

    update test

--- tests/Blazor.Diagrams.Tests/Components/Controls/ResizeControlWidgetTests.cs
+++ tests/Blazor.Diagrams.Tests/Components/Controls/ResizeControlWidgetTests.cs
@@ -1,23 +1,23 @@
 ﻿using Blazor.Diagrams.Components.Controls;
-using Blazor.Diagrams.Components.Renderers;
+using Blazor.Diagrams.Core.Controls.Default;
+using Blazor.Diagrams.Core.Positions.Resizing;
 using Bunit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
 using Xunit;
 
 namespace Blazor.Diagrams.Tests.Components.Controls
 {
-    public class ResizeControlWidgetTests
+	public class ResizeControlWidgetTests
     {
         [Fact]
         public void ShouldRenderDiv()
         {
             using var ctx = new TestContext();
-
-            var cut = ctx.RenderComponent<ResizeControlWidget>();
+            var providerMock = Mock.Of<IResizerProvider>();
+ 
+            var cut = ctx.RenderComponent<ResizeControlWidget>(parameters =>
+                parameters.Add(w => w.Control, new ResizeControl(providerMock))
+            );
 
             cut.MarkupMatches("<div class=\"default-node-resizer\" />");
         }
