2d9517bfae3ca796418a36d4a3f74a7a61f825fc
sam.peng <sam.peng@wisetechglobal.com>
2026-03-03 17:38:31 +0800

    Update DeploymentSetup.ps1 to change NuGet source to proget-gallery

--- DeploymentSetup.ps1
+++ DeploymentSetup.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $repoRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
 $nugetConfigPath = Join-Path $repoRoot 'nuget.config'
-$sourceName = 'WTG-Internal'
-$sourceUrl = 'https://proget.wtg.zone/nuget/WTG-Internal/v3/index.json'
+$sourceName = 'proget-gallery'
+$sourceUrl = 'https://proget.wtg.zone/nuget/Gallery/v3/index.json'
 
 dotnet nuget update source $sourceName --source $sourceUrl --configfile $nugetConfigPath | Out-Null
 if ($LASTEXITCODE -ne 0) {
